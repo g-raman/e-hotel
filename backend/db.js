649395eea3f22eb645ecaf48a4992c502377190b
aryangupta2 <ary.guptag@gmail.com>
2024-03-11 14:30:07 -0400

    minor security fix

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 const { Client } = require("pg");
+require("dotenv").config();
 
 const client = new Client({
     user: "postgres",
     host: "localhost",
     port: 5432,
-    password: "Aryan2004",
+    password: process.env.PASSWORD,
     database: "e-hotel"
 });
 
