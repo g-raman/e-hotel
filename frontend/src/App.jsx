e4408ba3958377eba52ed99e3132979e2898ee4a
Raman Gupta <gr.gupta.raman@gmail.com>
2024-03-23 19:04:02 -0400

    feat: add home page with basic search bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
+import Home from "./pages/Home";
+
 function App() {
-  return <div className="bg-red-500 text-xl">Hello world</div>;
+  return <Home />;
 }
 
 export default App;
---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,13 @@
+const SearchBar = () => {
+  return (
+    <div>
+      <input
+        className="p4 w-96 bg-slate-200"
+        type="text"
+        placeholder="Location"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
