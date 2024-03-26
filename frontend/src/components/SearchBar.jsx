50dde2b78fec5c113981c70399b18604cb5aae47
Raman Gupta <gr.gupta.raman@gmail.com>
2024-03-26 09:39:58 -0400

    fix: adjust sizing of input elements in search bar

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -5,19 +5,19 @@ import { DateRangePicker } from "./ui/daterangepicker";
 const SearchBar = () => {
   return (
     <div className="flex w-full min-w-0 items-end justify-center gap-4">
-      <div className="flex w-full flex-1 flex-col">
+      <div className="flex w-1/3 flex-col">
         <label className="my-2 text-gray-500" htmlFor="location">
           Location
         </label>
         <Input className="bg-white p-6" placeholder="Location" />
       </div>
 
-      <div className="flex flex-1 flex-col">
+      <div className="flex w-1/3 flex-col">
         <label className="my-2 text-gray-500">Check-in</label>
         <DateRangePicker className="w-full p-6" />
       </div>
 
-      <Button className="flex-1 p-6">Search</Button>
+      <Button className="w-1/3 p-6">Search</Button>
     </div>
   );
 };
