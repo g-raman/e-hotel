6f6dc57334a8f0c99b8f4d0c0a8cd5bcd9f2bd1a
Raman Gupta <gr.gupta.raman@gmail.com>
2024-03-25 21:40:51 -0400

    feat: add empty filter panel on results page

--- frontend/src/pages/Results.jsx
+++ frontend/src/pages/Results.jsx
@@ -3,14 +3,20 @@ import SearchBar from "@/components/SearchBar";
 
 const Results = () => {
   return (
-    <main>
-      <section className="mx-16 mt-10">
-        <SearchBar />
+    <main className="flex h-full w-full gap-4">
+      <section className="col-span-1 row-span-2 w-1/2 bg-slate-200">
+        Filters
       </section>
 
-      <section>
-        <HotelList />
-      </section>
+      <article className="flex flex-col gap-4">
+        <section className="col-start-2 row-start-1 row-end-1 mx-16 mt-10">
+          <SearchBar />
+        </section>
+
+        <section className="col-start-2 row-start-2">
+          <HotelList />
+        </section>
+      </article>
     </main>
   );
 };
