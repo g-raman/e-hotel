0e9677fc0c676e03fbcebfdc10e8edb5989cd80e
Raman Gupta <gr.gupta.raman@gmail.com>
2024-03-25 21:07:37 -0400

    feat: add more static information to result card

--- frontend/src/components/HotelCard.jsx
+++ frontend/src/components/HotelCard.jsx
@@ -2,6 +2,7 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "./ui/card";
@@ -10,8 +11,10 @@ const HotelCard = () => {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Hotel name</CardTitle>
-        <CardDescription>Room description</CardDescription>
+        <CardTitle className="text-2xl">Hotel name</CardTitle>
+        <CardDescription className="text-base">
+          Room description
+        </CardDescription>
       </CardHeader>
 
       <CardContent>
@@ -19,6 +22,16 @@ const HotelCard = () => {
           className="aspect-square w-full rounded-sm bg-auto object-cover "
           src="https://photos.hotelbeds.com/giata/bigger/00/004200/004200a_hb_ro_006.jpg"
         />
+
+        <div className="flex justify-between py-2 text-xl">
+          <span className="text-lg font-semibold">$49 per night</span>
+          <span>🌟 9.0/10</span>
+        </div>
+
+        <CardFooter className="flex flex-col items-start p-0">
+          <span>4 people</span>
+          <span>Sea view</span>
+        </CardFooter>
       </CardContent>
     </Card>
   );
