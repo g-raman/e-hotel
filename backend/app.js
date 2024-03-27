ed0641b99d95b5f5b33ae58f2f139c5f6387105f
Raman Gupta <gr.gupta.raman@gmail.com>
2024-03-26 23:11:56 -0400

    feat: add employee POST route

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,34 @@ app.post("/customers", (req, res) => {
       });
     }
   });
-  client.end;
+});
+
+app.post("/employees", (req, res) => {
+  const employee = req.body;
+  console.log(employee);
+
+  let insertQuery = `INSERT INTO public."Employee"(
+      "hotelID", "firstName", "lastName", "addressID", role, "SIN"
+    )
+    VALUES (
+      '${employee.hotelID}', '${employee.firstName}', '${employee.lastName}',
+      '${employee.addressID}', '${employee.role}', '${employee.SIN}'
+    );`;
+
+  client.query(insertQuery, (err, result) => {
+    if (!err) {
+      res.send("Employee Insertion Was Sucessful");
+    } else {
+      console.log("Employee Insertion was not successful");
+      console.log(err);
+      res.status(400).json({
+        data: {
+          status: "error",
+          message: "Employee Insertion was not successful",
+        },
+      });
+    }
+  });
 });
 
 module.exports = app;
