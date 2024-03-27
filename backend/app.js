dca756ad7afed72ff5882e717f8c59771791c337
Raman Gupta <gr.gupta.raman@gmail.com>
2024-03-27 18:43:59 -0400

    feat: implement cors

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const client = require("./db");
 const customerRouter = require("./routes/customerRoutes");
 const employeeRouter = require("./routes/employeeRoutes");
@@ -9,6 +11,8 @@ const hotelRouter = require("./routes/hotelRoutes");
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
+
 app.use("/api/v1/customers", customerRouter);
 app.use("/api/v1/employees", employeeRouter);
 app.use("/api/v1/reservations", reservationRouter);
