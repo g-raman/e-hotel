5dc74db25261db23ef6442a31479b1eaeac0fcae
Raman Gupta <gr.gupta.raman@gmail.com>
2024-03-27 14:33:55 -0400

    refactor: remove test route for employees

--- backend/app.js
+++ backend/app.js
@@ -10,32 +10,4 @@ app.use(bodyParser.json());
 app.use("/api/v1/customers", customerRouter);
 app.use("/api/v1/employees", employeeRouter);
 
-app.post("/employees", (req, res) => {
-  const employee = req.body;
-  console.log(employee);
-
-  let insertQuery = `INSERT INTO public."Employee"(
-      "hotelID", "firstName", "lastName", "addressID", role, "SIN"
-    )
-    VALUES (
-      '${employee.hotelID}', '${employee.firstName}', '${employee.lastName}',
-      '${employee.addressID}', '${employee.role}', '${employee.SIN}'
-    );`;
-
-  client.query(insertQuery, (err, result) => {
-    if (!err) {
-      res.send("Employee Insertion Was Sucessful");
-    } else {
-      console.log("Employee Insertion was not successful");
-      console.log(err);
-      res.status(400).json({
-        data: {
-          status: "error",
-          message: "Employee Insertion was not successful",
-        },
-      });
-    }
-  });
-});
-
 module.exports = app;
