fabf2afaac3acf2d5a81c6b6d03136e95a7b033a
Raman Gupta <gr.gupta.raman@gmail.com>
2024-03-27 18:49:48 -0400

    feat: move search bar to top of screen

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import SearchBar from "../components/SearchBar";
 
 const Home = () => {
   return (
-    <div className="flex h-dvh items-end justify-center bg-slate-50">
+    <div className="flex h-dvh items-start justify-center bg-slate-50">
       <div className="mx-16 my-10 w-full rounded-md bg-slate-200 px-16 py-10">
         <SearchBar />
       </div>
