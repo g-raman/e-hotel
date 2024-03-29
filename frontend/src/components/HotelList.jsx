c6c31954e06407e712ba9ce1644940bc6c4fccf1
Raman Gupta <gr.gupta.raman@gmail.com>
2024-03-29 19:26:06 -0400

    feat: add loading and error states for results

--- frontend/src/components/HotelList.jsx
+++ frontend/src/components/HotelList.jsx
@@ -1,5 +1,6 @@
 import { useSearch } from "@/contexts/SearchContext";
 import HotelCard from "./HotelCard";
+import Loader from "./Loader";
 
 const HotelList = () => {
   const { data, loading, error } = useSearch();
@@ -7,10 +8,20 @@ const HotelList = () => {
 
   return (
     <div className="grid grid-cols-3 gap-4 px-8 py-4">
-      {hotels?.length &&
+      {error && <p>{error}</p>}
+      {loading ? (
+        <div className="col-span-full row-span-1 flex size-32 w-full justify-center">
+          <Loader height={128} width={128} />
+        </div>
+      ) : hotels?.length === 0 ? (
+        <p className="col-span-full text-center text-muted-foreground">
+          No results found...
+        </p>
+      ) : (
         hotels.map((hotel) => {
           return <HotelCard key={hotel.roomID} info={hotel} />;
-        })}
+        })
+      )}
     </div>
   );
 };
--- frontend/src/pages/Results.jsx
+++ frontend/src/pages/Results.jsx
@@ -5,12 +5,12 @@ import SearchBar from "@/components/SearchBar";
 const Results = () => {
   return (
     <main className="flex h-full w-full gap-4">
-      <section className="col-span-1 row-span-2 w-1/2 bg-slate-200 px-8 py-12">
+      <section className="col-span-1 row-span-2 w-1/3 bg-slate-200 px-8 py-12">
         <h2 className="text-3xl font-bold">Filters</h2>
         <FilterList />
       </section>
 
-      <article className="flex flex-col gap-4">
+      <article className="flex h-full w-full flex-col gap-4">
         <section className="col-start-2 row-start-1 row-end-1 mx-8 mt-10">
           <SearchBar />
         </section>
