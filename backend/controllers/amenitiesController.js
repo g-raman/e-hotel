e07a547a503e32dd444b7e2cf3add3fd7060d71b
Raman Gupta <gr.gupta.raman@gmail.com>
2024-03-29 22:50:24 -0400

    feat: return amenities in alphabetical order

--- backend/controllers/amenitiesController.js
+++ backend/controllers/amenitiesController.js
@@ -2,7 +2,7 @@ const db = require("../db");
 const catchAsync = require("../utils/catchAsync");
 
 exports.getAllAmenities = catchAsync(async (req, res, next) => {
-  const query = `SELECT DISTINCT "amenity" FROM "RoomAmenities"`;
+  const query = `SELECT DISTINCT "amenity" FROM "RoomAmenities" ORDER BY "amenity" ASC`;
 
   try {
     const results = await db.query(query);
