909f91533c199ed50b0eeaaf5d444b1b8f385f53
Raman Gupta <gr.gupta.raman@gmail.com>
2024-03-31 16:48:58 -0400

    feat: allow employee to add payment details

--- frontend/src/components/CheckInButton.jsx
+++ frontend/src/components/CheckInButton.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { Button } from "./ui/button";
 import { useFetch } from "@/hooks/useFetch";
+import PaymentDialog from "./PaymentDialog";
 
 const fetchOptions = {
   method: "PATCH",
@@ -15,13 +15,50 @@ const CheckInButton = ({ id }) => {
   const [shouldConvert, setShouldConvert] = useState({ current: false });
   const endpoint = CONVERT_TO_RENTING_ENDPOINT + id;
 
+  const [details, setDetails] = useState({
+    creditCardNumber: "",
+    creditCardExpiry: "",
+    creditCardCVV: "",
+  });
+
+  function handleChange(e) {
+    const elem = e.target.id;
+    const value = e.target.value;
+    let isValid = false;
+
+    if (value === "") isValid = true;
+
+    if (!isValid && isNaN(parseInt(value))) return;
+
+    if (elem === "card") {
+      setDetails((details) => {
+        return { ...details, creditCardNumber: e.target.value };
+      });
+    } else if (elem === "expiry") {
+      setDetails((details) => {
+        return { ...details, creditCardExpiry: e.target.value };
+      });
+    } else if (elem === "cvv") {
+      setDetails((details) => {
+        return { ...details, creditCardCVV: e.target.value };
+      });
+    }
+  }
+
   useFetch(endpoint, shouldConvert, {}, fetchOptions);
 
   function handleClick() {
+    fetchOptions.body = JSON.stringify({ ...details });
     setShouldConvert({ current: true });
   }
 
-  return <Button onClick={handleClick}>Check-In</Button>;
+  return (
+    <PaymentDialog
+      details={details}
+      onChange={handleChange}
+      onSubmit={handleClick}
+    />
+  );
 };
 
 export default CheckInButton;
---
+++ frontend/src/components/PaymentDialog.jsx
@@ -0,0 +1,74 @@
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+  DialogHeader,
+} from "./ui/dialog";
+
+const PaymentDialog = ({ details, onSubmit, onChange }) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Check-in</Button>
+      </DialogTrigger>
+
+      <DialogContent className="">
+        <DialogHeader>
+          <DialogTitle>Add Payment</DialogTitle>
+          <DialogDescription>Ask user for their credit card</DialogDescription>
+        </DialogHeader>
+        <form id="payment" className="grid grid-cols-2 gap-4">
+          <div className="col-span-full">
+            <label className="text-muted-foreground">Credit Card Number</label>
+            <Input
+              value={details.creditCardNumber}
+              id="card"
+              onChange={onChange}
+              required
+              placeholder="XXXX XXXX XXXX XXXX "
+              minlength={16}
+              maxlength={16}
+            />
+          </div>
+
+          <div>
+            <label className="text-muted-foreground">Expiry Date</label>
+            <Input
+              value={details.creditCardExpiry}
+              id="expiry"
+              onChange={onChange}
+              required
+              placeholder="MMYY"
+              minlength={4}
+              maxlength={4}
+              type="number"
+            />
+          </div>
+
+          <div>
+            <label className="text-muted-foreground">CVV</label>
+            <Input
+              value={details.creditCardCVV}
+              id="cvv"
+              onChange={onChange}
+              required
+              placeholder="CVV"
+              minlength={3}
+              maxlength={3}
+              type="number"
+            />
+          </div>
+          <Button form="payment" className="col-span-full" onClick={onSubmit}>
+            Done
+          </Button>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default PaymentDialog;
